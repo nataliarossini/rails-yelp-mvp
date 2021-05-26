a2195a831b7708519d022a8a95f2b2cd9ad5ae76
Natalia Rossini <naty.rossini@hotmail.com>
2021-05-26 16:58:58 +0100

    all mvc done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
 5.times do
   puts 'Creating restaurant'
   Restaurant.create(
     name: Faker::Games::Pokemon.name,
     address: Faker::Movies::HarryPotter.location,
-    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    # rating: rand(1..5)
   )
   puts 'Restaurant created'
 end
